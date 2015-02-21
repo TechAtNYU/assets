e3d2a2a6070dd18fec5aa29fbe537e2381bf5fd5
Abhi Agarwal <me@abhiagarwal.com>
2015-02-21 12:42:00 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+  , app = express()
+  , bodyParser = require('body-parser')
+  , jsonParser = bodyParser.json()
+  , exec = require('child_process').exec
+  , sh = require('execSync').run;
+
+var directoryForGitHook = "/root/services"
+  , restartCommand = "forever restartall";
+
+app.post('/githook', jsonParser, function (req, res) {
+  // This to make sure the request comes from Github
+  if('before' in req.body) {
+    sh("cd " + directoryForGitHook);
+    sh("git pull -u origin master");
+    sh(restartCommand);
+    res.send('Complete!');
+  }
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+});
