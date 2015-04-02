ec0fcb783bc464783c83f4ee3030743a7c1b34a3
Abhi Agarwal <me@abhiagarwal.com>
2015-04-01 21:06:23 -0400

    Update proxy-api.js

--- proxy/proxy-api.js
+++ proxy/proxy-api.js
@@ -1,31 +1,41 @@
-// targetURL is removed for safety.
-
 var http = require('http'),
-    fs             = require('fs'),
-    path           = require('path'),
+    fs = require('fs'),
+    path = require('path'),
     httpProxy = require('http-proxy'),
     https = require('https'),
-    apiProxy = httpProxy.createProxyServer();
+    express = require('express'),
+    app = express();
+
+var apiProxy = httpProxy.createProxyServer();
 
 http.createServer(function (req, res) {
   var targetURL = '';
+  console.log(req.url);
   if (req.url.indexOf("v1.0") > -1){
-    targetURL = '';
-  } else if(req.url.indexOf("v2") > -1) {
     targetURL = '';
   } else {
     targetURL = '';
   }
-  apiProxy.web(req, res, { target: targetURL });
-}).listen(process.env.port);
+  apiProxy.web(req, res, {
+    target: targetURL,
+    xfwd: true
+  });
+}).listen(3000);
+
+app.get('*', function (req, res) {
+  res.redirect('https://api.tnyu.org' + req.url)
+})
+var server = app.listen(8080, '0.0.0.0');
 
 httpProxy.createServer({
   target: {
-    host: process.env.host,
-    port: process.env.port
+    host: 'localhost',
+    port: 3000
   },
+  xfwd: true,
   ssl: {
-    key: fs.readFileSync(path.resolve(__dirname, process.env.KeyDir), 'utf8'),
-    cert: fs.readFileSync(path.resolve(__dirname, process.env.CrtDir), 'utf8')
-  }
+    key: fs.readFileSync(path.resolve(__dirname, ''), 'utf8'),
+    cert: fs.readFileSync(path.resolve(__dirname, ''), 'utf8')
+  },
+  secure: true
 }).listen(443);
