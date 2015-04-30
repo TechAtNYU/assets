0e4a56f6dbe1fd78b8811fdd81290d1c6a823656
Abhi Agarwal <me@abhiagarwal.com>
2015-04-29 21:22:25 -0400

    Adding jshintrc, jscrc, and fixing

--- circleci-build/index.js
+++ circleci-build/index.js
@@ -1,23 +1,19 @@
-var exec = require('child_process').exec
-  , sh = require('execSync').run
-  , CircleCI = require('circleci');
+'use strict';
+
+var CircleCI = require('circleci');
 
 var ci = new CircleCI({
-  auth: process.env.CircleCIKey
+	auth: process.env.CircleCIKey
 });
 
-var runBuild = function(){
-  ci.startBuild({
-    username: "TechAtNYU",
-    project: process.env.ProjectName,
-    branch: process.env.ProjectBranch
-  }).then(function(build){
-    console.log(build);
-  });
+var runBuild = function() {
+	ci.startBuild({
+		username: 'TechAtNYU',
+		project: process.env.ProjectName,
+		branch: process.env.ProjectBranch
+	}).then(function(build) {
+		console.log(build);
+	});
 };
 
 runBuild();
-
-//setTimeout(function() { 
-//  runBuild(); 
-//}, 3600000);
--- githook/index.js
+++ githook/index.js
@@ -1,24 +1,25 @@
-var express = require('express')
-  , app = express()
-  , bodyParser = require('body-parser')
-  , jsonParser = bodyParser.json()
-  , exec = require('child_process').exec
-  , sh = require('execSync').run;
+'use strict';
 
-var directoryForGitHook = "/root/services"
-  , restartCommand = "forever restartall";
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var sh = require('execSync').run;
 
-app.post('/githook', jsonParser, function (req, res) {
-  // This to make sure the request comes from Github
-  if('before' in req.body) {
-    sh("cd " + directoryForGitHook + " && git pull -u origin master");
-    sh("cd " + directoryForGitHook + " && " + restartCommand);
-    res.send('Complete!');
-  }
+var directoryForGitHook = '/root/services';
+var restartCommand = 'forever restartall';
+
+app.post('/githook', jsonParser, function(req, res) {
+	// This to make sure the request comes from Github
+	if ('before' in req.body) {
+		sh('cd ' + directoryForGitHook + ' && git pull -u origin master');
+		sh('cd ' + directoryForGitHook + ' && ' + restartCommand);
+		res.send('Complete!');
+	}
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port)
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
 });
